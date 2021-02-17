4efd774882e8d85ae4ff28fa3fff050741b19086
Anoop Verma (LCL-C) <Anoop.Verma3@loblaw.ca>
2021-02-17 14:56:55 -0500

    Updating QA and Prod IPs in test files.

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.15.149.39:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.15.22.197:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
