1c7e04a3a15c35eb712006fd7a0a0e17500d0f30
Anoop Verma (LCL-C) <Anoop.Verma3@loblaw.ca>
2021-03-02 02:06:42 -0500

    Updating IPs and jenkins file.

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.21.46.178:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://172.31.1.112:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
